6d3143d030205ca7355942cb00ff09c9c54cad37
ebruaeo <ebruaeo@hotmail.com>
2024-02-18 00:49:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/contextprojesi/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.contextprojesi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Toast , Alert
+
+
+    }
+
+    fun mesajGoster(view: View) {
+
+    }
+}
